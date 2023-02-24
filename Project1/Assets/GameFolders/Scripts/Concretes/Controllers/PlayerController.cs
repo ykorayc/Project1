9e7f77579434091217317db734c22e255423b566
ykorayc <104435682+ykorayc@users.noreply.github.com>
2023-02-25 01:09:34 +0300

    ScriptingStarting
    PlayerControllerScriptWriting

---
+++ Project1/Assets/GameFolders/Scripts/Concretes/Controllers/PlayerController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Project1.Controllers
+{
+    public class PlayerController : MonoBehaviour
+    {
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+
+        }
+    }
+
+}
---
+++ Project1/Assets/GameFolders/Scripts/Concretes/Inputs/DefaultInput.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Project1.Inputs
+{
+    public class DefaultInput : MonoBehaviour
+    {
+        
+    }
+}
+
