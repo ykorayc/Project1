bf49f4ce74e7b0df9af8c40e7cadfff0da0b3a1c
ykorayc <104435682+ykorayc@users.noreply.github.com>
2023-02-25 15:53:24 +0300

    PlayerControllerisDone
    Player Controller Scipt Basically done.

--- Project1/Assets/GameFolders/Scripts/Concretes/Controllers/PlayerController.cs
+++ Project1/Assets/GameFolders/Scripts/Concretes/Controllers/PlayerController.cs
@@ -1,19 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Project1.Inputs;
 
 namespace Project1.Controllers
 {
     public class PlayerController : MonoBehaviour
     {
-        void Start()
+        Rigidbody _rigidbody;
+        DefaultInput _input;
+        bool isForceUp;
+        [SerializeField] private float _force;
+        private void Awake()
         {
+            _rigidbody = GetComponent<Rigidbody>();
+            _input = new DefaultInput();
+        }
+        private void Update()
+        {
+            if (_input.isForceUp)
+            {
+                isForceUp = true;
+            }
+            else
+            {
+                isForceUp = false;
+            }
 
         }
-
-        void Update()
+        private void FixedUpdate()
         {
-
+            if(isForceUp)
+            {
+                _rigidbody.AddForce(Vector3.up*Time.deltaTime*_force,ForceMode.Force);
+            }
         }
     }
 
--- Project1/Assets/GameFolders/Scripts/Concretes/Inputs/DefaultInput.cs
+++ Project1/Assets/GameFolders/Scripts/Concretes/Inputs/DefaultInput.cs
@@ -3,9 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 namespace Project1.Inputs
 {
-    public class DefaultInput : MonoBehaviour
+    public class DefaultInput
     {
-        
+        private DefaultAction _input;
+        public bool isForceUp { get; private set; }
+        public DefaultInput()
+        {
+            _input = new DefaultAction();
+            _input.Rocket.MoveUp.performed += context => isForceUp = context.ReadValueAsButton();
+            _input.Enable();
+        }
     }
 }
 
